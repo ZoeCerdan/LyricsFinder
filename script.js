32e2b7513f071ff0e2963215fab3fa849f10cccc
Zoé Cerdan <zoecerdan21@outlook.fr>
2025-01-07 14:28:18 +0100

    Deuxième commit

--- script.js
+++ script.js
@@ -1,38 +1,45 @@
-// 1) Masquer la loupe au chargement de la page
-window.onload = function () {
-    document.getElementById("loader").style.display = "none";
+    // 1) Masquer la loupe au chargement de la page
+    window.onload = function () {
+        document.getElementById("loader").style.display = "none";
     };
 
 
-// 2) Capturer l'évènement d'envoi du formulaire et annuler la redirection
-document.getElementById('searchForm').addEventListener('submit', function (r) {
-    r.preventDefault();
-    
+    // 2) Capturer l'évènement d'envoi du formulaire et annuler la redirection
+    document.getElementById('search-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        
+        const lyricsContainer = document.getElementById('lyrics');
+        
+        // 5) Désactiver le bouton d'envoi du formulaire, masquer le texte et afficher la loupe
+        document.getElementsByName("submit").disabled = true;
+        lyricsContainer.textContent = '';
+        document.getElementById("loader").style.display = 'inline'; 
 
-    const artist = document.getElementById('artist').value;
-    const title = document.getElementById('title').value;
-    // Remplacer les espaces par des tirets 
-    const formattedTitle = title.replace(/\s+/g, '-'); 
+        const artist = document.querySelector("input[name=artist]").value;
+        const title = document.querySelector("input[name=title]").value;
 
-// 3) Rechercher les paroles via l'API et les afficher à l'écran
-var lyricsContainer = document.getElementById('lyrics');
-const UrlAPI =  https.//lyrics.api.pierre-jehan.com
-fetch(UrlAPI)
-.then(response => response.json())
-.then(data => {
+        // Remplacer les espaces par des tirets 
+        const newtitle = title.replace(/\s/g, '-'); 
 
-    const songs = data['hydra:member'];
-    const lyrics = songs[0].lyrics;
-    if (lyrics) {
-        document.getElementById('lyrics').innerText = lyrics;
-    } else {
-        document.getElementById('lyrics').innerText = 'The music you are looking for cant be found';
-    }
-})
+        // 3) Rechercher les paroles via l'API et les afficher à l'écran
+        const UrlAPI = 'https://lyrics.api.pierre-jehan.com/music?title=' + newtitle + '&artist=' + artist;
+        
+        fetch(UrlAPI)
+        .then(response => response.json())
+        .then(data => {
 
+            const songs = data['hydra:member'];
+            if (songs.length > 0) {
+                const lyrics = songs[0].lyrics;
 
- // 5) Désactiver le bouton d'envoi du formulaire, masquer le texte et afficher la loupe
-document.getElementsByName("submit").style.display = "true";
- lyricsContainer.textContent = '';
- document.getElementById("loader").style.display = 'inline';  
-   
+                document.getElementById('lyrics').innerText = lyrics;
+            } else {
+                document.getElementById('lyrics').innerText = 'The music you are looking for cant be found';
+            }
+
+            // TODO: Réactiver le bouton
+            document.getElementsByName("submit").disabled = false;
+            // Masquer la loupe une fois les paroles affichées
+            document.getElementById("loader").style.display = "none";
+        }) 
+    });
